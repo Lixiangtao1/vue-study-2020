43f6acb9a0c48334745947fd35e263c622e1a4b7
unknown <1549854846@qq.com>
2020-03-15 23:39:04 +0800

    webpack代码提交

---
+++ 05-Webpack的使用/01-webpack的起步/src/main.js
@@ -0,0 +1,13 @@
+// 入口文件
+
+// 需求： 在这里使用add mul 函数打印
+//1. 使用Common.js模块化的规范
+const { add, mul } = require ('./mathUtil.js'); // 引入mathUtil.js文件
+console.log(add(10,20));
+console.log(mul(10,20));
+
+// 2.使用ES6的模块化规范
+import { name, age, height } from './info'
+console.log(name);
+console.log(age);
+console.log(height);
